985f2e2f02b9d8bdb0435290643ac0aca415d676
Chris Chinolla <chris@MacBook-Pro.lan>
2024-01-02 16:49:37 -0700

    added main content container and nav bar, made it responsive to smaller screen sizes

---
+++ components/mainpage.js
@@ -0,0 +1,18 @@
+import styles from '../styles/main.module.css'
+import Image from 'next/image';
+
+export default function MainPage() {
+    return (
+
+        <div className={styles.main}>
+            <aside className={styles.sidecontent}>
+
+            </aside>
+            <section className={styles.maincontent}>
+
+            </section>
+        </div>
+
+
+    );
+}
